cf1abcc02e931e15e2065427786ae05453bc6133
Miguel Garcia Dolagaray <miguel.garcia9@upr.edu>
2016-08-19 06:48:55 -0400

    Completed exercise 1.3

--- first.py
+++ first.py
@@ -5,10 +5,29 @@ table = survey.Pregnancies()
 table.ReadRecords()
 print 'Number of pregnancies', len(table.records)
 
-liveBirths = 0
+liveFirstBirths = 0
+liveOtherBirths = 0
+lengthOf1stPregnancy=0;
+lengthOfOtherPregnancy=0;
+
 
 for pregnancy in table.records:
 	#print pregnancy.outcome
-	if pregnancy.outcome==1 : liveBirths=liveBirths+1
+	if pregnancy.outcome==1 :
+		if pregnancy.birthord == 1 : 
+			liveFirstBirths = liveFirstBirths+1
+			lengthOf1stPregnancy=lengthOf1stPregnancy+pregnancy.prglength
+		else : 
+			liveOtherBirths = liveOtherBirths+1
+			lengthOfOtherPregnancy=lengthOfOtherPregnancy+pregnancy.prglength
 
-print 'Number of Live Births: ', liveBirths
+print 'Number of Live Births: ', liveFirstBirths + liveOtherBirths
+print 'Number of Live First Births: ', liveFirstBirths
+print 'Number of Live Other Births: ', liveOtherBirths
+print 'Average 1st Pregnancy', lengthOf1stPregnancy/liveFirstBirths
+print 'Average Other Pregnancy', lengthOfOtherPregnancy/liveOtherBirths
+
+#Average pregnancy length
+
+
+
